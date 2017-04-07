e1dadf1f76f564ef9878129fd4ff38eef3a72e16
ecschwab <schwabe543@gmail.com>
2017-04-07 17:35:37 -0400

    added user model to database

--- fintech/newsletter/models.py
+++ fintech/newsletter/models.py
@@ -4,20 +4,20 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 # Create your models here.
-#class SiteUser(models.Model):
-#    user = models.OneToOneField(User,on_delete=models.CASCADE)
-#    email = models.TextField(max_length=25, blank=False)
-#    password = models.TextField(max_length=25, blank=False)
-#    usertype = models.CharField(choices = (('c','Company User'),('i','Investor User'),('m','Site Manager'),), max_length=1)
+class SiteUser(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    email = models.TextField(max_length=25, blank=False)
+    password = models.TextField(max_length=25, blank=False)
+    usertype = models.CharField(choices = (('c','Company User'),('i','Investor User'),('m','Site Manager'),), max_length=1)
 
-#    @receiver(post_save,sender=User)
-#    def create_user(sender,instance,created,**kwargs):
-#        if created:
-#            SiteUser.objects.create(user=instance)
-
-#   @receiver(post_save,sender=User)
-#    def save_user(sender,instance,**kwargs):
-#        instance.siteuser.save()
+    @receiver(post_save,sender=User)
+    def create_user(sender,instance,created,**kwargs):
+        if created:
+            SiteUser.objects.create(user=instance)
+            
+    @receiver(post_save,sender=User)
+    def save_user(sender,instance,**kwargs):
+       instance.siteuser.save()
     
 
 
